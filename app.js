38e150d131424754aa5d1688d22868d468cd0535
Virgile <virgilejoinville@gmail.com>
2024-11-21 22:35:43 +0100

    adding cors

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 
 // Middleware pour parser les données JSON
 app.use(express.json());
-
+app.use(cors({
+    origin: '*', // Permet toutes les origines
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Autorise toutes les méthodes
+    allowedHeaders: ['Content-Type', 'Authorization'] // Autorise des en-têtes spécifiques
+}));
 // Route POST pour logger les données reçues
 app.post('/log', (req, res) => {
     console.log('Donnée reçue:', req.body); // Log les données dans la console
