70db2b74b483dc104bfeddcb60ac50d90364d4ce
Joinville <v.joinville@uxco-group.com>
2024-11-22 16:34:53 +0100

    adding logic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const transformDataController = require('./src/controllers/transformDataController');
+const transformDataController = require('./src/controllers/dataController');
 
 
 // Middleware pour parser les données JSON
