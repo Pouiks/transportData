fa8752af730ec5b6632723652b66b97a45496b88
Joinville <v.joinville@uxco-group.com>
2024-11-25 10:46:21 +0100

    adding API key

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -1,21 +1,16 @@
-const https = require('https'); // Module pour configurer l'agent HTTPS
 const data = require('./data.json');
 require('dotenv').config();
 
-// Création d'un agent HTTPS qui ignore la vérification du certificat
-const agent = new https.Agent({
-    rejectUnauthorized: false, // Désactive la vérification SSL
-});
 
 const sendDataToClient = async (dataToSend) => {
-    console.log(data);
+    console.log(dataToSend);
     fetch(process.env.CLIENT_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': process.env.API_KEY
         },
-        body: JSON.stringify(dataToSend), // Convertir en JSON string
-        agent, // Ajout de l'agent HTTPS pour ignorer le certificat
+        body: dataToSend // Convertir en JSON string
     })
         .then(response => response.json())
         .then(data => console.log('Réponse du client:', data))
